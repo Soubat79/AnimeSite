80ac6b7bd21c7dfa30b34f326c32cbf3e1c77819
Soubat79 <145777333+Soubat79@users.noreply.github.com>
2025-11-06 21:43:46 +0530

    Implement service worker with caching strategy

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
+const CACHE_NAME = 'animeflix-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/anime/naruto.html',
+  '/anime/onepiece.html',
+  '/anime/demon-slayer.html',
+  '/anime/attack-on-titan.html'
+];
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
